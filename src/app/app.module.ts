75c053c17286125f13e6ec8c15bf465fbd162002
Glakuta <glakuta@gmail.com>
2023-08-19 23:14:58 +0300

    Done body header, progress in filters and home bod

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,9 +21,12 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from "@angular/common/http";
 import { HeaderComponent } from "./components/header/header.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
+import { FiltersComponent } from './pages/home/components/filters/filters.component';
+import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent, FiltersComponent, ProductBoxComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,23 @@
 import { Component } from "@angular/core";
 
+const ROW_HEIGHT: { [id: number]: number } = { 1: 400, 3: 355, 4: 350 };
+
 @Component({
   selector: "app-home",
   templateUrl: "./home.component.html",
   styles: [],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  cols = 3;
+  rowHeight = ROW_HEIGHT[this.cols];
+  category: string | undefined;
+
+  onColumnsCountChange(colsNum: number): void {
+    this.cols = colsNum;
+    this.rowHeight = ROW_HEIGHT[this.cols];
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
+  }
+}
