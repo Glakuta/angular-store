82981c4dbed30d91e26cc9cfe28816ca8120b3ed
Glakuta <glakuta@gmail.com>
2023-08-20 16:29:09 +0300

    Done home page, create cart

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,7 +1,10 @@
-import { Component } from "@angular/core";
+import {Component, Input} from "@angular/core";
 
 @Component({
   selector: "app-product-box",
   templateUrl: "./product-box.component.html",
 })
-export class ProductBoxComponent {}
+export class ProductBoxComponent {
+  @Input() fullWidthMode = false
+
+}
